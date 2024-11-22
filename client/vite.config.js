73dab4f1bc841a535aa41d397cd5a4826259fea9
Hashem Elmaghrabi <hashe@MacBook-Pro.lan>
2024-11-21 21:37:31 -0500

    Fixed import bug in SavedBooks.jsx; created dist folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  },
   server: {
     port: 3000,
     open: true,
